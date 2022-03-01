f3f9a7c6cb69bff3423a0d87980a1352d0ace42e
Marcelo Hauch Piske <marcelopiske@ideyas.it>
2022-03-01 16:52:01 -0300

    subindo versao dark e correçao de titulos

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const sideMenu = document.querySelector('aside');
 const menuBtn = document.querySelector('#menu-btn');
 const closeBtn = document.querySelector('.close');
+const themeToggler = document.querySelector(".theme-toggler")
 
+// show sidebar
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
 })
 
+// close sidebar
 closeBtn.addEventListener('click', () => {
     sideMenu.style.display = 'none';
+})
+
+// change theme
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables')
+
+    themeToggler.querySelector('span:nth-chield(1)').classList.toggle('active')
+    themeToggler.querySelector('span:nth-chield(2)').classList.toggle('active')
 })
